80f412d9ec17a940132f52618f26ab3b501e7f4d
Limsangkeun <dlatkdrms301@gmail.com>
2022-09-23 09:24:47 +0900

    Select 테스트 완료

--- src/test/java/com/junitproject/domain/BookRespositoryTest.java
+++ src/test/java/com/junitproject/domain/BookRespositoryTest.java
@@ -2,8 +2,9 @@ package com.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Optional;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,8 +12,23 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 public class BookRespositoryTest {
 
+    private final String PRE_TITLE = "사전준비 타이틀";
+    private final String PRE_AUTHOR = "임상근";
+
     @Autowired
     private BookRepository repository;
+
+    @BeforeEach
+    public void preset_data () {
+
+        Book newBook = Book.builder()
+                        .title(PRE_TITLE)
+                        .author(PRE_AUTHOR)
+                        .build();
+
+        repository.save(newBook);
+    }
+    
     
     // 1. 책 등록
     @Test
@@ -36,7 +52,26 @@ public class BookRespositoryTest {
 
     // 2. 책 목록 보기
 
+    @Test
+    public void findBookList() {
+        List<Book> bookList = repository.findAll();
+
+        assertEquals(1, bookList.size());
+    }
+
     // 3. 책 한권 보기
+    
+    @Test
+    public void findBookById() {
+        // given
+        
+        // when 
+        Book book = repository.findById(1L).get();
+
+        // then
+        assertEquals(PRE_TITLE, book.getTitle());
+        assertEquals(PRE_AUTHOR, book.getAuthor());
+    }
 
     // 4. 책 수정하기
 
