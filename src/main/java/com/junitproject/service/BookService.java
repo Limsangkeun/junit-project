9969e6e79fc7461f2c7fc1e2b4a1fb6fcefe4e80
Limsangkeun <dlatkdrms301@gmail.com>
2022-09-23 10:55:39 +0900

    bookService의 저장, 전체 목록 조회 작성

--- src/main/java/com/junitproject/service/BookService.java
+++ src/main/java/com/junitproject/service/BookService.java
@@ -1,5 +1,35 @@
 package com.junitproject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.junitproject.domain.Book;
+import com.junitproject.domain.BookRepository;
+import com.junitproject.web.dto.BookRespDto;
+import com.junitproject.web.dto.BookSaveReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class BookService {
+
+    @Autowired
+    private final BookRepository bookRepository;
     
+    // 1. 책 등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto saveBook(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return BookRespDto.toDto(bookPS);
+    }
+
+    // 2. 책 목록 보기
+
+    // 3. 책 한권 보기
+
+    // 4. 책 삭제 하기
+    
+    // 5. 책 수정 하기
 }
--- src/main/java/com/junitproject/web/dto/BookRespDto.java
+++ src/main/java/com/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,28 @@
 package com.junitproject.web.dto;
 
+import com.junitproject.domain.Book;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookRespDto {
-    
+    Long id;
+    String title;
+    String author;
+
+    private BookRespDto (Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public static BookRespDto toDto (Book bookPS) {
+        return new BookRespDto(bookPS.getId(), bookPS.getTitle(), bookPS.getAuthor());
+    }
 }
